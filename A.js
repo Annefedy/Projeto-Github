92eaa1865c5ad7ea17c70fe3b3795497d9284b1c
Annefedy <103973102+Annefedy@users.noreply.github.com>
2022-08-06 20:50:54 -0300

    Alteração na cidade

--- A.js
+++ A.js
@@ -1,6 +1,6 @@
 var informacao = {
     Nome:"Anne Carine Bien Aime",
-    Cidade: "Sao paulo",
+    Cidade: "Sao paulo SP",
     bio: function() {
         alert(this.nome[0]+ ' ' +this.nome[1] + ' tem ' + this.cidade + ' Voce mora em qual cidade ' + ' . ');
     },
