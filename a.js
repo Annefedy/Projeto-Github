2acddb7298839c882c38d267b8894831f1f5608f
Annefedy <103973102+Annefedy@users.noreply.github.com>
2022-08-06 20:20:58 -0300

    Meu primeiro commit

---
+++ a.js
@@ -0,0 +1,10 @@
+var informacao = {
+    Nome:"Anne Carine Bien Aime",
+    Cidade: "Sao paulo",
+    bio: function() {
+        alert(this.nome[0]+ ' ' +this.nome[1] + ' tem ' + this.cidade + ' Voce mora em qual cidade ' + ' . ');
+    },
+    saudacao: function() {
+        alert('Ola! Eu sou' + this.nome[0] + ' . ');
+    }
+};
